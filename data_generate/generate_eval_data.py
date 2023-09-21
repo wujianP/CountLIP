61af1a9c6af471d236ae0fc0d82f3b40c18a7788
wujianP <1051233700@qq.com>
2023-09-21 12:57:59 +0800

    explore dataset

--- data_generate/generate_eval_data.py
+++ data_generate/generate_eval_data.py
@@ -1,17 +1,49 @@
 import wandb
 import os
+import random
+import json
 
-image_root = '/DDN_ROOT/wjpeng/dataset/FSC-147/images_384_VarV2'
-ann_root = ''
+from PIL import Image
+
+img_root = '/DDN_ROOT/wjpeng/dataset/FSC-147/images_384_VarV2'
+ann_root = '/DDN_ROOT/wjpeng/dataset/FSC-147/annotations'
 
 
 def explore_fsc147():
+    
+    img_filenames = os.listdir(img_root)
+    random.shuffle(img_filenames)
+
+    img2cls_path = os.path.join(ann_root, 'ImageClasses_FSC147.txt')
+    img2cls = {}
+    with open(img2cls_path, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            img, cls = line.strip().split('\t')
+            img2cls[img] = cls
+    f.close()
+
+    ann_path = os.path.join(ann_root, 'annotation_FSC147_384.json')
+    with open(ann_path, 'r') as f:
+        anns = json.load(f)
+    f.close()
+
+    for img_filename in img_filenames:
+        path = os.path.join(img_root, img_filename)
+        img = Image.open(path).convert('RGB')
+        cls = img2cls[img_filename]
+        ann = anns[img_filename]
+        points = ann['points']
+        count = len(points)
+        ann['count'] = count
+
+        run.log({'FSC-147': wandb.Image(img, caption=f'class: {cls}  -- count: {count}')})
+
     from IPython import embed
     embed()
-    img_filenames = os.listdir(image_root)
-
 
 
 if __name__ == '__main__':
     wandb.login()
+    run = wandb.init('FSC-147')
     explore_fsc147()
---
+++ data_generate/run_generate_eval_data.sh
@@ -0,0 +1,5 @@
+conda activate /discobox/wjpeng/env/clip
+cd /discobox/wjpeng/code/202306/CountLIP/data_generate/
+git pull
+
+python generate_eval_data.py
