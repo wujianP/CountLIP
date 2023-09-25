f0d8bfcfc85ebe3883c13615bb586b8d52851a17
wujianP <1051233700@qq.com>
2023-09-26 00:39:21 +0800

    BLIP caption ImageNet

--- data_generate/download_google_benchmark.py
+++ data_generate/download_google_benchmark.py
@@ -13,8 +13,6 @@ if __name__ == '__main__':
     output_dir = '/DDN_ROOT/wjpeng/dataset/countBench/google/data'
     os.makedirs(output_dir, exist_ok=True)
     p = inflect.engine()
-    for i in range(1, 11):
-        os.makedirs(os.path.join(output_dir, p.number_to_words(i+1)))
 
     with open(dataset_file, 'r') as file:
         dataset_raw = json.load(file)
@@ -26,26 +24,26 @@ if __name__ == '__main__':
         number = entry['number']
         number_word = p.number_to_words(number)
 
-        response = requests.get(image_url)
-
-        if response.status_code == 200:
+        try:
+            raw_image = Image.open(requests.get(image_url, stream=True).raw).convert('RGB')
             # 提取文件名
-            image_filename = os.path.join(output_dir, number_word, f'{per_number_cnt[number]:02d}.jpg')
-            ann_filename = os.path.join(output_dir, number_word, f'{per_number_cnt[number]:02d}.json')
+            image_filename = os.path.join(output_dir, f'{number_word}_{per_number_cnt[number]:02d}.jpg')
+            ann_filename = os.path.join(output_dir, f'{number_word}_{per_number_cnt[number]:02d}.json')
             per_number_cnt[number] += 1
 
             # 保存图片
-            with open(image_filename, 'wb') as img_file:
-                img_file.write(response.content)
+            raw_image.save(image_filename)
             # 保存文本
-            with open(ann_filename, 'wb') as txt_file:
+            with open(ann_filename, 'w') as txt_file:
                 ret = {
                     'number': number,
+                    'number_word': number_word,
                     'text': text,
                 }
-                json.dumps(ret, indent=4)
-        else:
-            print(f"下载失败：{image_url}")
+                json.dump(ret, txt_file, indent=4)
+        except:
+            print(f"下载失败：{image_url}"
+
         print(f'[{idx + 1} / {len(dataset_raw)} ({(idx + 1) / len(dataset_raw) * 100:.2f} %)]')
 
     print(f'下载完成：'
