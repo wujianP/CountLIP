eb14d52b7a9426329c158639805dc7492f2968c3
wujianP <1051233700@qq.com>
2023-09-26 11:28:34 +0800

    evaluate on Google Benchmark

---
+++ evaluate/run_google_countBench.sh
@@ -0,0 +1,9 @@
+conda activate /discobox/wjpeng/env/countLIP
+cd /discobox/wjpeng/code/202306/CountLIP/evaluate
+
+python main_google_benchmark.py \
+--data-root='/DDN_ROOT/wjpeng/dataset/countBench/google/data' \
+--batch-size=16 \
+--num-workers=8 \
+--model-name="ViT-B-32" \
+--resume=''
