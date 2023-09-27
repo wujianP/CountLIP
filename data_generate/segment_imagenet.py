a6a4d8b397d1b0767b9b09879b90df03a58b3f5e
wujianP <1051233700@qq.com>
2023-09-27 13:57:57 +0800

    evaluate on Google Benchmark

--- data_generate/segment_imagenet.py
+++ data_generate/segment_imagenet.py
@@ -3,8 +3,9 @@ from torch.utils.data import DataLoader
 
 import torch
 import argparse
-
+import wandb
 import numpy as np
+import matplotlib.pyplot as plt
 
 # segment anything
 from segment_anything import build_sam, SamPredictor
@@ -44,6 +45,50 @@ def prepare_sam_data(images, boxes, resize_size):
     return batched_input
 
 
+def show_mask(mask, ax, random_color=True):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+
+
+def show_box(box, ax, label, random_color=True):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor=color, facecolor=(0, 0, 0, 0), lw=2))
+    ax.text(x0, y0, label)
+
+
+def wandb_visualize(images, class_names, boxes, masks):
+    for img, cls, box, mask in zip(images, class_names, boxes, masks):
+        w, h = img.size
+        # box, img, mask
+        plt.figure(figsize=(w / 80, h / 80))
+        ax1 = plt.gca()
+        ax1.axis('off')
+        ax1.imshow(img)
+        show_box(box, ax1, cls)
+        show_mask(mask, ax1)
+        fig1 = plt.gcf()
+        plt.close()
+
+        # mask only
+        plt.figure(figsize=(w / 80, h / 80))
+        ax2 = plt.gca()
+        ax2.axis('off')
+        show_mask(mask, ax2)
+        fig2 = plt.gcf()
+
+        run.log({'segment': [wandb.Image(fig1), wandb.Image(fig2)]})
+
+
 @torch.no_grad()
 def main():
 
@@ -66,10 +111,10 @@ def main():
     for cur_iter, (images, boxs, class_names, class_ids, filenames) in enumerate(dataloader):
         # prepare sam input
         batched_input = prepare_sam_data(images=images, boxes=boxs, resize_size=sam.image_encoder.img_size)
-        from IPython import embed
-        embed()
         batched_output = sam(batched_input, multimask_output=False)
         masks_list = [output['masks'].cpu().numpy() for output in batched_output]
+        from IPython import embed
+        embed()
 
 
 if __name__ == '__main__':
@@ -80,4 +125,8 @@ if __name__ == '__main__':
     parser.add_argument('--sam_checkpoint', type=str, default='/discobox/wjpeng/weights/sam/sam_vit_h_4b8939.pth')
     args = parser.parse_args()
     device = 'cuda'
+
+    wandb.login()
+    run = wandb.init('SAM ImageNet')
     main()
+    run.finish()
