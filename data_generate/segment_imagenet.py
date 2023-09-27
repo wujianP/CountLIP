4ab77231229e76cdfa7ee43e9f57a8f9c7e8bea1
wujianP <1051233700@qq.com>
2023-09-27 13:47:50 +0800

    evaluate on Google Benchmark

--- data_generate/segment_imagenet.py
+++ data_generate/segment_imagenet.py
@@ -34,7 +34,7 @@ def prepare_sam_data(images, boxes, resize_size):
     batched_input = []
     for i in range(len(images)):
         w, h = images[i].size
-        box = torch.tensor([boxes[i]])
+        box = torch.tensor([boxes[i]]).cuda()
         data = {
             'image': prepare_image(images[i], resize_transform),
             'boxes': resize_transform.apply_boxes_torch(box, (h, w)),
