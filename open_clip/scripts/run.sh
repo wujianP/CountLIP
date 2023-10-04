a91769fe2c72a4fe8b35beab4f15a05d6fb6e7b9
wujianP <1051233700@qq.com>
2023-10-04 15:57:14 +0800

    evaluate on Google Benchmark

--- open_clip/scripts/run.sh
+++ open_clip/scripts/run.sh
@@ -1,27 +1,30 @@
 #--val-data
 #
-#conda activate /discobox/wjpeng/env/countLIP
-#cd /discobox/wjpeng/code/202306/CountLIP/open_clip/src/training
-#git pull
-#rm -rf /DDN_ROOT/wjpeng/ckp/CountLIP/debug
-#CUDA_VISIBLE_DEVICES=0 python -m main \
-#    --dataset-type="count" \
-#    --count-loss-type="intra" \
-#    --count-loss-weight=1. \
-#    --data-root /dev/shm/imagenet \
-#    --train-data="zhan-wei-fu" \
-#    --hard-num=2 \
-#    --empty-fill-type="mean" \
-#    --logs="/DDN_ROOT/wjpeng/ckp/CountLIP/debug" \
-#    --name="countLIP-debug-2" \
-#    --batch-size=128 \
-#    --epochs=5000 \
-#    --lr=5e-7 \
-#    --pretrained="laion2b_s34b_b79k" \
-#    --model="ViT-B-32"\
-#    --workers 8 \
-#    --copy-codebase \
-#    --warmup 50
+conda activate /discobox/wjpeng/env/countLIP
+cd /discobox/wjpeng/code/202306/CountLIP/open_clip/src/training
+git pull
+rm -rf /DDN_ROOT/wjpeng/ckp/CountLIP/debug
+CUDA_VISIBLE_DEVICES=0 python -m main \
+    --dataset-type="webdataset" \
+    --train-data="/DDN_ROOT/wjpeng/dataset/LAION400M/laion400m-data-00-01/{00000..02586}.tar" \
+    --train-num-samples=100000 \
+    --count-loss-type="intra" \
+    --count-loss-weight=1. \
+    --data-root /dev/shm/imagenet \
+    --train-data="zhan-wei-fu" \
+    --hard-num=2 \
+    --empty-fill-type="mean" \
+    --logs="/DDN_ROOT/wjpeng/ckp/CountLIP/debug" \
+    --name="countLIP-debug" \
+    --batch-size=128 \
+    --epochs=5000 \
+    --lr=5e-7 \
+    --pretrained="laion2b_s34b_b79k" \
+    --model="ViT-B-32"\
+    --model-root="/discobox/wjpeng/weights/clip" \
+    --workers 8 \
+    --copy-codebase \
+    --warmup 50
 
 
 
@@ -33,7 +36,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 torchrun --nproc_per_node 8 -m main \
     --segmented-object \
     --logs="/DDN_ROOT/wjpeng/ckp/CountLIP/waiting" \
-    --name="seg-real-bg-1e-6" \
+    --name="seg-real-bg-1e-6_" \
     --dataset-type="count" \
     --count-loss-type="inter" \
     --count-loss-weight=1. \
