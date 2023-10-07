558bc7d17e5d400ed61a471e346fbfb1e0ec3574
wujianP <1051233700@qq.com>
2023-10-07 17:50:43 +0800

    存档-开始修改代码以支持omni-source data training: 1. data相关的部分初步写完了，还没整体跑通

--- open_clip/scripts/debug.sh
+++ open_clip/scripts/debug.sh
@@ -2,7 +2,8 @@ conda activate /discobox/wjpeng/env/countLIP
 cd /discobox/wjpeng/code/202306/CountLIP/open_clip/src/training
 git pull
 rm -rf /DDN_ROOT/wjpeng/ckp/CountLIP/debug
-CUDA_VISIBLE_DEVICES=0 python -m main \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+torchrun --nproc_per_node 8 -m main \
     --dataset-type="mix" \
     --train-data="/DDN_ROOT/wjpeng/dataset/LAION400M/laion400m-data-00-01/{00000..02587}.tar" \
     --train-num-samples=-1 \
@@ -10,16 +11,16 @@ CUDA_VISIBLE_DEVICES=0 python -m main \
     --empty-fill-type="black" \
     --segmented-object \
     --count-loss-type="inter" \
-    --count-loss-weight=1. \
-    --batch-size=128 \
-    --count-batch-size=8 \
+    --count-loss-weight=0.5 \
+    --batch-size=256 \
+    --count-batch-size=32 \
     --hard-num=2 \
-    --epochs=5000 \
-    --steps-per-epoch=500 \
+    --epochs=10 \
+    --steps-per-epoch=1000 \
     --lr=1e-6 \
     --logs="/DDN_ROOT/wjpeng/ckp/CountLIP/debug" \
     --name="countLIP-debug" \
     --pretrained="openai" \
     --model="ViT-B-32"\
     --copy-codebase \
-    --warmup 5000
+    --warmup 1000
