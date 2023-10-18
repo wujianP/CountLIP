33b4271089c4e6638c9d267040ac87ff611b1cfa
wujianP <1051233700@qq.com>
2023-10-18 21:36:27 +0800

    test

--- data_generate/scripts/run_segment_imagenet.sh
+++ data_generate/scripts/run_segment_imagenet.sh
@@ -1,4 +1,4 @@
-JOB_ID=0
+JOB_ID=7
 conda activate /discobox/wjpeng/env/countLIP
 cd /discobox/wjpeng/code/202306/CountLIP/data_generate
 git pull
@@ -6,9 +6,9 @@ git pull
 export CUDA_VISIBLE_DEVICES=$((JOB_ID % 8))
 python segment_imagenet.py \
 --data_root='/dev/shm/imagenet/' \
---out_dir='/DDN_ROOT/wjpeng/dataset/imagenet/segment_sam_vit_h' \
+--out_dir='/DDN_ROOT/wjpeng/dataset/instance_pool/imagenet_sam_vit_h' \
 --batch_size=8 \
 --num_workers=8 \
 --sam_checkpoint='/discobox/wjpeng/weights/sam/sam_vit_h_4b8939.pth' \
---job_num=16 \
+--job_num=8 \
 --job_id=$JOB_ID
