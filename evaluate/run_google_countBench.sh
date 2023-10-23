60dcc1ecf0028e990fd8fc89ed97e20c6a963cc3
wujianP <1051233700@qq.com>
2023-10-23 12:31:54 +0800

    添加了wandb

--- evaluate/run_google_countBench.sh
+++ evaluate/run_google_countBench.sh
@@ -7,6 +7,9 @@ python main_google_benchmark.py \
 --batch-size=16 \
 --num-workers=8 \
 --model-name="ViT-B-32" \
---resume='/workspace/b32_400m.pt'
+--resume='/DDN_ROOT/wjpeng/ckp/betterClip/debug/DDN_ROOT/wjpeng/ckp/betterClip/debug/checkpoints/epoch_1.pt'
+
+--resume='/DDN_ROOT/wjpeng/ckp/betterClip/debug/DDN_ROOT/wjpeng/ckp/betterClip/debug/checkpoints/epoch_1.pt'
+
 --resume='/DDN_ROOT/wjpeng/ckp/CountLIP/imagenet_only/inter_hn-2_fill-black_bs256*8_ep10_lr5e-6_warm100_vit-b-32-openai/checkpoints/epoch_5.pt'
 
