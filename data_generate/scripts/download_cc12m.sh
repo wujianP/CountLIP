6aeba78ad4bddcd0c895a1f1a8fcb8fbd7898700
wujianP <1051233700@qq.com>
2023-11-01 12:14:37 +0800

    test

--- data_generate/scripts/download_cc12m.sh
+++ data_generate/scripts/download_cc12m.sh
@@ -4,5 +4,5 @@ conda activate /discobox/wjpeng/env/clip/
 cd /DDN_ROOT/wjpeng/dataset/cc12m/
 
 img2dataset --url_list cc12m.tsv --input_format "tsv"\
-         --url_col "url" --caption_col "caption" --output_format webdataset\
-           --output_folder data --processes_count 16 --thread_count 64 --resize_mode no
+         --url_col "url" --caption_col "caption" --output_format webdataset \
+         --output_folder data --processes_count 32 --thread_count 128 --resize_mode no --incremental
