5011056bf8e2f73aba4ecea8b6e0075564eec6f8
wujianP <1051233700@qq.com>
2023-11-03 18:27:34 +0800

    test

--- evaluate/run_google_countBench.sh
+++ evaluate/run_google_countBench.sh
@@ -7,4 +7,4 @@ python main_google_benchmark.py \
 --batch-size=64 \
 --num-workers=8 \
 --model-name="ViT-B-32" \
---resume='/DDN_ROOT/wjpeng/ckp/betterCLIP/ep1000-step100-warm10000_lr1e-6_common-bs256-cc3m-cc12m_extra-bs8-hn2-wt0.25/checkpoints/epoch_20.pt'
+--resume='/DDN_ROOT/wjpeng/ckp/betterCLIP/new-vitb-32-openai_ep100-step100-warm2000_lr1e-6_common-bs256-cc3m-cc12m_extra-bs8-hn2-wt0.25/checkpoints/epoch_40.pt'
